810486af76cb3db3ee57613c36d08169cd540a7b
Fnyasimi <nfestus@yahoo.com>
2019-05-25 17:58:13 +0300

    Removed the text wrapper module                                                                                                                                                                                     Represented the information without the text wrapper module and also managed the error if the amino acid is not in the dictionary to be represented by X

--- Scripts/information.py
+++ Scripts/information.py
@@ -6,8 +6,7 @@ def pdb_info(path):
     number of sheets and the amino acid sequence of the protein.
     Arguments : the path to the pdb file."""
 
-    global typec
-    import textwrap # a module for formating text in python
+    global typec # distinguish between amino acids and nucleotide
 
     # Creating an amino acid dictionary with the 3 letter code as key and the ambiguous code as value
     aminoacids_dict = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D','CYS':'C','GLU':'E','GLN':'Q','GLY':'G','HIS':'H',
@@ -29,7 +28,6 @@ def pdb_info(path):
             if line.startswith("SEQRES"):
                 
                 seq_line = line.split() #split the line into fields 
-
                 chain = seq_line[2] #select the chains from the respective column
 
                 if chain not in p_chains:
@@ -38,29 +36,27 @@ def pdb_info(path):
 
         # creating the title and formating it
         print("\n")
-        main_head = "TITLE"
-        prefix = main_head + ": " # add the separator
-        preferredWidth = 80 #linewidth from start to end 
+        main_head = "Title: "
+        titlelist = []
+        len_main_head = len(main_head)
+        for i in range(0,len(header),74):
+            ln = (" "*len_main_head)+ header[i:i+74]
+            titlelist.append(ln)
 
-        title_wrapper = textwrap.TextWrapper(initial_indent=prefix, width=preferredWidth,
-                                           subsequent_indent=' '*len(prefix))
-
-        if len(p_chains) <= 2:
-            chains_title = "CHAINS: " + " and ".join(p_chains) #join all the chains together in the subtitle
-    
-        else:
-            chains_title = "CHAINS: " + ", ".join(p_chains[:-1])
-            chains_title = chains_title + " and " + "".join(p_chains[-1]) #join all the chains together in the subtitle
-
-        print(title_wrapper.fill(header)) # printing the title
-        print(chains_title) #print the chains present
-        #print(aa_count)   
-
+        line1 = titlelist[0]
+        line1 = list(line1)
+        line1[0:len_main_head]=main_head
+        line1 = "".join(line1)
+        titlelist[0] = line1
+        for line in titlelist:
+            print(line)
+        
     # loop through the protein chains to extract sequence information and secondary structure info
     for chain in p_chains: #using the list of chains obtained as the iteratable
         aminoacid_seq = "" #initialize a blank amino acid string
         helix_count = 0 #initialize helix counter
         sheet_count = 0 #initialize sheet counter
+        
 
         with open (path, "r") as myfile:
             for line in myfile:
@@ -71,7 +67,7 @@ def pdb_info(path):
 
                     for amino in sp_line[4:]:
                         if len(amino)==3:
-                            amino1_letter = aminoacids_dict.get(amino)
+                            amino1_letter = aminoacids_dict.get(amino, "X") #if amino acid not in dictionary put X
                             aminoacid_seq = aminoacid_seq + "".join(amino1_letter)
                             typec = "amino acids:"
                         else:
@@ -89,15 +85,23 @@ def pdb_info(path):
             helixcount = "      Number of helix:" + (" "*(len(aminoacids_count) - 22 - len(str(helix_count)))) + str(helix_count)
             sheetcount = "      Number of sheet:" + (" "*(len(aminoacids_count) - 22 - len(str(sheet_count)))) + str(sheet_count)
 
-            #format the sequence paragraph into a wrap
-            seqs ="      Sequence"
-            prefix = seqs + ": "
-            preferredWidth = 66 #lenth of prefix(16) plus preffered width size of amino acid sequence(50 characters)
-            seq_wrapper = textwrap.TextWrapper(initial_indent=prefix, width=preferredWidth,
-                                           subsequent_indent=' '*len(prefix))
+            #format the sequence paragraph split the lines to the length of 50
+            linelist = []
+            s_title = "      Sequence: "
+            len_s_title = len(s_title)
+            for i in range(0,len(aminoacid_seq),50):
+                ln = (" "*len_s_title)+ aminoacid_seq[i:i+50]
+                linelist.append(ln)
+
+            line1 = linelist[0] #select line 1 from list
+            line1 = list(line1) # cast it as list
+            line1[0:len_s_title]=s_title #add sequence title
+            line1 = "".join(line1) #join it
+            linelist[0] = line1 #add it back to the list
 
             print(" - "+ "Chain "+ chain)
             print(aminoacids_count)
             print(helixcount)
             print(sheetcount)
-            print(seq_wrapper.fill(aminoacid_seq))
+            for line in linelist:
+                print(line)
